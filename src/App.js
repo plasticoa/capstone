7f05704e0b60a390e63789cbeef801bd24c765e0
Allan Alonzo <2988752920101@ingenieria.usac.edu.gt>
2022-07-30 09:10:07 -0600

    -added home and shop routes -added Outlet router inside home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import CategoryItemComponent from "./components/category-item/CategoryItem.component";
+import Home from "./pages/home/Home.component";
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
 
+  const Shop = () => {
+    return (
+      <h2>Shop component</h2>
+    )
+  }
+
   return (
-    <div>
-      <CategoryItemComponent />
-    </div>
+    <Routes>
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+
   );
 }
-
 export default App;
